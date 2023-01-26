c1786633ef2bd2d4179f56f80f393c0aecf1649c
arifremal <arifremal013@gmail.com>
2023-01-26 18:56:13 +0600

    js done

---
+++ reminder.js
@@ -0,0 +1,5 @@
+var mangos =11;
+var eta =2;
+//  / this mean vaggol
+//  % this mean vagshesh
+console.log(mangos % eta);
---
+++ types.js
@@ -0,0 +1,12 @@
+// var price1 =100;
+// console.log( typeof price1)
+
+// var isHappy =true;
+// console.log(typeof isHappy)
+
+var num1 = 0.2;
+var num2= 0.1;
+var sum = num1 + num2;
+sum = sum.toFixed(2);
+sum = parseFloat(sum)
+console.log(sum )
