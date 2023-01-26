69dace22eb0db38fd0c288210d4582781eef5c34
arifremal <arifremal013@gmail.com>
2023-01-26 18:42:45 +0600

    js journey start

---
+++ concatination.js
@@ -0,0 +1,11 @@
+var price1 = '31';
+var price2 = 10;
+var to = price1 + ' '+ price2;
+console.log(to);
+// convert string to number
+
+var price1Number = parseInt(price1);
+console.log(price1Number);
+var gpa ='3.45';
+var gpaNumber = parseFloat(gpa);
+console.log(gpaNumber)
---
+++ conversation.js
@@ -0,0 +1,9 @@
+// var price1 = 31;
+// var price2 = 10;
+// var totallPrice = price1 + price2;
+// console.log(totallPrice)
+
+var firstName = 'ar';
+var lastName = 'remal';
+var fullNmae = firstName + ' '+ lastName;
+console.log(fullNmae)
